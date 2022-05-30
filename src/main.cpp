afa0193ef4279296db32ea5c17ea3e43aa30e35e
Andy Barnett <andy.abtec@gmail.com>
2022-05-30 08:36:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
    License:    GNU 3.0
 
    Detects a wired tool being used or a remote fob being pressed to control a shop vac with
-   a 10A relay.
+   a solid state relay.
   */
 
 // Include Libaries...
@@ -18,44 +18,72 @@
 #define DEBUG 1
 
 // Initialize delay times...
-
-#define onToolDelay 3                               // Delay between detecting the tool and
-                                                    // nergising the vac relay
+#define onToolDelay 3                               // Delay between detecting the tool turning
+                                                    // on and benergising the vac relay
 #define offDelayMin 3                               // Value sets the minimum off time delay
 #define offDelayMax 30                              // Value sets the maximum off time delay
 #define offDelayPin A7                              // pot connection to adjust the off time delay
 
-int offDelay()                                      //  Calculate the off time delay based on the min
-{                                                   //  and max delays and the pot setting
-  int x=analogRead(offDelayPin);
-  int offDelay=round((offDelayMax-offDelayMin)/1032.0*x+offDelayMin);
-  return(offDelay);
-}
-
 // Define Pins...
 #define remoteInt 2                                 // ISR trigger for remote
-#define remoteState 4                               // Current state of remote
 #define toolInt 3                                   // ISR trigger for tool
-#define toolState 5                                 // Current tool state
-
 #define relay 7                                     // Relay
 #define power 12                                    // Red "Power" LED
 #define vac 11                                      // Yellow "Vacuum" LED
 #define tool 10                                     // Green "Tool" LED
 #define remote 9                                    // Blue "Remote" LED
 
+#define vacLedFlashsPerSecond 4                     // Shutdown flash rate
+
 // Define Flags...
-bool remoteFlag=false;
+int remoteFlag=0;
 bool toolFlag=false;
 
+// Calculate the off time delay based on the min and max delays and the pot setting
+int offDelay(){
+int x=analogRead(offDelayPin);
+int offDelay=round((offDelayMax-offDelayMin)/1032.0*x+offDelayMin);
+return(offDelay);
+}
+
 // Flash the vacuum led...
-void flash(int flashesPerSecond, int seconds)
-{
+  void flash(int flashesPerSecond, int seconds){
   int rate=500/flashesPerSecond;
   for(int i=0; i<flashesPerSecond*seconds*2; i++)
   {
     digitalWrite(vac,not(digitalRead(vac)));
     delay(rate);
+  }
+}
+
+// Interrupt Service Routines...
+void remoteISR(void){
+  #if DEBUG==1
+    printf("in Remote ISR\n");
+  #endif
+  remoteFlag=digitalRead(remoteInt);
+  #if DEBUG==1
+    printf("Remote Flag = %d\n",remoteFlag);
+  #endif
+}
+
+void toolISR(void);
+
+// Start Remote...
+
+// Start Tool...
+
+// Stop...
+void stop(void){
+  if(remoteFlag==false && toolFlag==false)
+  {
+    noInterrupts();
+    flash(vacLedFlashsPerSecond,offDelay());
+    digitalWrite(relay,false);
+    digitalWrite(vac,false);
+    digitalWrite(remote,false);
+    digitalWrite(tool,false);
+    interrupts();
   }
 }
 
@@ -86,17 +114,18 @@ void setup()
     digitalWrite(relay,LOW);
   #endif
 
-  #if DEBUG==1                                      // DEBUG: Read the offDelay
+  #if DEBUG==1                                      // DEBUG: print the offDelay
     printf("offDelay = %d\n",offDelay());
-    delay (2500);
   #endif
 
-  // Setup complete, turn on the power led...
+attachInterrupt(remoteInt, remoteISR, CHANGE);
+attachInterrupt(toolInt, toolISR, CHANGE);
+
+// Setup complete, turn on the power led...
   digitalWrite(power,HIGH);
 }
 
-// Loop...
+  // Loop...
 void loop()
 {
-
 }
